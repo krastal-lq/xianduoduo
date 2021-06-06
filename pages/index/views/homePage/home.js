c69df0de5abf12ed1d18bb4c3e9c7ce8a4deaa6d
krastal-lq <1811375953@qq.com>
2021-06-06 19:52:17 +0800

    更新

---
+++ pages/index/views/homePage/home.js
@@ -0,0 +1,49 @@
+// pages/home/pages/home.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    swiper: [
+      'https://img13.360buyimg.com/babel/s1420x960_jfs/t1/137619/20/8754/126775/5f641dd4Ef9250fa5/3c1e14895f42caef.jpg!cc_1420x960.webp',
+      'https://img13.360buyimg.com/babel/s1420x960_jfs/t1/132255/11/5541/76551/5f1e6d10E98a2e832/8c9c5fd6acf32e8e.png!cc_1420x960.webp',
+      'https://img11.360buyimg.com/babel/s1420x960_jfs/t1/132846/11/6851/179116/5f334ffdEda434cfe/99d008d50f83096a.png!cc_1420x960.webp',
+      'https://img30.360buyimg.com/babel/s1420x960_jfs/t1/122421/7/8067/75888/5f1e6cf0E30279d66/895260b7a591fe62.png!cc_1420x960.webp'
+    ],
+    menus: [{
+        icon: '/images/iconimg/youxuan.png',
+        title: '优选商城',
+        color:'#BEE570'
+      },
+      {
+        icon: '/images/iconimg/nongshi.png',
+        title: '逛农市',
+        color:'#F8CEB4'
+      },
+      {
+        icon: '/images/iconimg/zhengce.png',
+        title: '看政策',
+        color:'#9DBAE1'
+      },
+      {
+        icon: '/images/iconimg/funong.png',
+        title: '消费帮扶',
+        color:'#B4DDFF'
+      }
+    ],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
